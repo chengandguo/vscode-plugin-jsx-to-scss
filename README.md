4a5f4e96c6c803a6e2cc0bd5e76dc41486c45b36
成 真 <cz275512@lazada.com>
2020-11-22 18:07:42 +0800

    feat: init project

---
+++ README.md
@@ -0,0 +1,14 @@
+# jsx-to-scss README
+This is a jsx-to-scss plugin which can help you extract scss from jsx template.
+
+## Features
+- 1.Select the JSX you want to extract class names
+- 2.Show your context Menu, and click "JSX Extract Style to Clipboard"(or use cmd/ctrl + e)
+- 3.Paste the SCSS structure to your *.scss file.
+
+### For more information
+
+* [QQ Mail](1649424609@qq.com)
+
+
+
---
+++ utils/utils.js
@@ -0,0 +1,113 @@
+const vscode = require('vscode');
+const copyPaste = require("copy-paste");
+const jsxParser = require('./jsxParser');
+let {
+  JSXParser
+} = require("./jsxParser");
+
+
+const editor = vscode.window.activeTextEditor;
+
+// 获取当前选中的文本
+function getSelectedText() {
+  let doc = editor.document.getText();
+  let arr = doc.split("\n");
+  let {
+    start,
+    end
+  } = editor.selection;
+  let str = "";
+  for (let i = start.line; i <= end.line; ++i) {
+    if (i === start.line) {
+      str += arr[i].slice(start.character);
+    } else if (i === end.line) {
+      str += arr[i].slice(0, end.character);
+    } else {
+      str += arr[i];
+    }
+  }
+  return str;
+}
+
+// 提取样式
+function extractStyle(text = "") {
+  let obj = JSXParser(text);
+  if (!obj) {
+    vscode.window.showErrorMessage("Please check your JSX syntax");
+    return false;
+  } else {
+    return generateSass(obj);
+  }
+
+}
+
+/*
+  {
+    type: "div",
+    props: {
+      className: "abc",
+      id: "demo"
+    },
+    children: [
+
+    ]
+  }
+*/
+const DEFAULT_SPACE_COUNT = 2;  // 默认缩进为2个空格
+function generateSass(obj, spaceCount = 0) {
+  let result = "";
+  let {
+    props,
+    children
+  } = obj;
+
+  if (props.className) {
+    let classNames = props.className.trim().split(" ");
+    result += generateWhiteSpace(spaceCount);
+    let arr = [];
+    for (let item of classNames) {
+      if (!item) continue;
+      arr.push(`.${item}`)
+    }
+    result += arr.join(", ") + " {\n";
+  }
+
+  if (children.length) {
+    let prefix = "";
+    let count = props.className ? DEFAULT_SPACE_COUNT : 0;
+    for (let [index, item] of children.entries()) {
+      prefix = (index === 0 || !item.props.className) ? "" : "\n";
+      result += prefix + generateSass(item, spaceCount + count);
+    }
+  }
+
+  if(props.className) {
+    result += "\n" + generateWhiteSpace(spaceCount) + "}"
+  }
+
+  return result;
+}
+
+// 生成指定数量的空格
+function generateWhiteSpace(cnt) {
+  let result = "";
+  for (let i = 0; i < cnt; ++i) {
+    result += " ";
+  }
+  return result;
+}
+
+
+// 写入剪贴板
+function writeToClipboard(text) {
+  copyPaste.copy(text, () => {
+    vscode.window.showInformationMessage("Scss copys to clipboard successfully!");
+  });
+}
+
+
+module.exports = {
+  getSelectedText,
+  extractStyle,
+  writeToClipboard,
+}
